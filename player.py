23c1759025ff618df05e667571dd8bcbe82d6344
Jafus1 <pierro118@hotmail.com>
2024-12-13 18:09:16 -0500

    Player is slightly less overpowered

--- player.py
+++ player.py
@@ -4,6 +4,7 @@ from constants import *
 
 class Player(CircleShape):
     containers = None
+    timer = 0 # To limit firing speed. 
 
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
@@ -29,12 +30,15 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self, dt):
-        bullet = Shot(self.position.x, self.position.y, SHOT_RADIUS)
-        bullet.velocity = (pygame.Vector2(0, 1).rotate(self.rotation)) * PLAYER_SHOOT_SPEED
+        if self.timer < 0: 
+            bullet = Shot(self.position.x, self.position.y, SHOT_RADIUS)
+            bullet.velocity = (pygame.Vector2(0, 1).rotate(self.rotation)) * PLAYER_SHOOT_SPEED
+            self.timer = PLAYER_SHOOT_COOLDOWN
 
     def update(self, dt):
-        keys = pygame.key.get_pressed()
+        self.timer -= dt
 
+        keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
             self.rotate(-dt)
         if keys[pygame.K_d]:
