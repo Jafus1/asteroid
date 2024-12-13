b753a0a1239752f89564d3c90236bd42d4806b15
Jafus1 <pierro118@hotmail.com>
2024-12-13 15:42:13 -0500

    Player is now drawn

--- main.py
+++ main.py
@@ -4,12 +4,14 @@
 import pygame
 # import the constants variable from constants.py
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         # Check if players pressed the X in the corner. 
         for event in pygame.event.get():
@@ -17,10 +19,11 @@ def main():
                 return
 
         screen.fill((0, 0, 0)) # RGB value for black.
+        player.draw(screen)
         pygame.display.flip()
 
+        # Limit the framerate to 60 FPS. 
         dt = clock.tick(60) / 1000
-        print(dt)
-
+        
 if __name__ == "__main__":
     main()
