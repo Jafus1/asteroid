3e8cdee2df13cce20fc06975cec9aad01bbd322b
Jafus1 <pierro118@hotmail.com>
2024-12-13 16:34:24 -0500

    Added the player sprite into updatable and drawable group

--- main.py
+++ main.py
@@ -11,17 +11,28 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    print(updatable)
+
     while True:
         # Check if players pressed the X in the corner. 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
-        
+        # player.update(dt)
+        for object in updatable:
+            object.update(dt)
+
         screen.fill((0, 0, 0)) # RGB value for black.
-        player.draw(screen)
+        # player.draw(screen)
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
 
         # Limit the framerate to 60 FPS. 
